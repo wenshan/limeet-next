20bfd714651174df89c6ee191c0929b9d11c88c2
wenshan <hou_ve@hotmail.com>
2025-09-05 08:35:32 +0800

    fix: .next

---
+++ .next/static/chunks/src_app_home_[lang]_page_jsx_2e143843._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_e5430a5f._.js",
+  "static/chunks/src_0eb7a835._.js",
+  "static/chunks/src_821b6343._.css"
+],
+    source: "dynamic"
+});
